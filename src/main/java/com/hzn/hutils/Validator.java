107e32b151a9410f907044933e97f49002c90517
hzn <gjwnssud@gmail.com>
2024-10-25 13:20:26 +0900

    feat : validator 완료

--- src/main/java/com/hzn/hutils/Validator.java
+++ src/main/java/com/hzn/hutils/Validator.java
@@ -1,5 +1,7 @@
 package com.hzn.hutils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.ValidatorFactory;
@@ -7,6 +9,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>jakarta validation-api 유틸</p>
@@ -14,14 +18,26 @@ import java.util.stream.Collectors;
  * @author hzn
  * @date 2024. 10. 24.
  */
-public class Validator {
+public final class Validator {
+	private static final Logger logger = LoggerFactory.getLogger (Validator.class);
 
-	public static Optional<Map<String, String>> validate (Class<?> clazz) {
+	private Validator () {
+	}
+
+	public static <T> Optional<Map<String, String>> validate (T t) {
 		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory ()) {
 			jakarta.validation.Validator validator = factory.getValidator ();
-			Set<ConstraintViolation<Class<?>>> violations = validator.validate (clazz);
+			Set<ConstraintViolation<T>> violations = validator.validate (t);
 			if (!violations.isEmpty ()) {
-				return Optional.of (violations.stream ().collect (Collectors.toMap (v -> v.getPropertyPath ().toString (), ConstraintViolation::getMessage)));
+				Map<String, String> violationMap = violations.stream ()
+				                                             .collect (Collectors.toMap (v -> v.getPropertyPath ().toString (), ConstraintViolation::getMessage));
+				try {
+					ExceptionLog.print ("validate", new RuntimeException (new ObjectMapper ().writeValueAsString (violationMap)), logger);
+				} catch (JsonProcessingException e) {
+					ExceptionLog.print ("validate", e, logger);
+					throw new RuntimeException (e);
+				}
+				return Optional.of (violationMap);
 			}
 		}
 		return Optional.empty ();
