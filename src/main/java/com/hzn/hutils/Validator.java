db5c054f42d0bfa01372f0759483a728508448d8
hzn <gjwnssud@gmail.com>
2024-10-25 15:38:31 +0900

    fix : Validator 수정

--- src/main/java/com/hzn/hutils/Validator.java
+++ src/main/java/com/hzn/hutils/Validator.java
@@ -1,12 +1,10 @@
 package com.hzn.hutils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.ValidatorFactory;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -24,22 +22,19 @@ public final class Validator {
 	private Validator () {
 	}
 
-	public static <T> Optional<Map<String, String>> validate (T t) {
+	public static <T> void validate (T t) throws Exception {
 		try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory ()) {
 			jakarta.validation.Validator validator = factory.getValidator ();
 			Set<ConstraintViolation<T>> violations = validator.validate (t);
 			if (!violations.isEmpty ()) {
 				Map<String, String> violationMap = violations.stream ()
-				                                             .collect (Collectors.toMap (v -> v.getPropertyPath ().toString (), ConstraintViolation::getMessage));
-				try {
-					ExceptionLog.print ("validate", new RuntimeException (new ObjectMapper ().writeValueAsString (violationMap)), logger);
-				} catch (JsonProcessingException e) {
-					ExceptionLog.print ("validate", e, logger);
-					throw new RuntimeException (e);
-				}
-				return Optional.of (violationMap);
+				                                             .collect (Collectors.toMap (v -> v.getPropertyPath ().toString (), ConstraintViolation::getMessage,
+				                                                                         (existingValue, newValue) -> newValue));
+				String violationsString = new ObjectMapper ().writeValueAsString (violationMap);
+				Exception e = new Exception (violationsString);
+				ExceptionLog.print ("validate", e, logger);
+				throw e;
 			}
 		}
-		return Optional.empty ();
 	}
 }
